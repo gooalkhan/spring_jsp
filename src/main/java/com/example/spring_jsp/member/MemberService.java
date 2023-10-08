7c94341a93a11dc56482eb63525a094a33208c30
gooalkhan <gooalkhan@gmail.com>
2023-10-08 10:29:32 +0900

    오타 수정 파이썬 h2접근 확인

---
+++ src/main/java/com/example/spring_jsp/member/MemberService.java
@@ -0,0 +1,4 @@
+package com.example.spring_jsp.member;
+
+public interface MemberService {
+}
--- src/main/resources/python/main.py
+++ src/main/resources/python/main.py
@@ -5,7 +5,7 @@ import os
 URI = "jdbc:h2:tcp://localhost:9092/mem:testdb"
 ID = "sa"
 PW = ""
-LIB_PATH = os.getcwd() + "/build/resources/main/python/lib/h2-2.1.214.jar"
+LIB_PATH = os.getcwd() + "\\build\\resources\\main\\python\\lib\\h2-2.2.224.jar"
 
 try:
     import jaydebeapi
@@ -17,9 +17,9 @@ finally:
     try:
         print("%s process starts" % sys.argv[0])
 
-        print("file exists %s" % os.path.exists(LIB_PATH))
+        print("file exists %s in %s" % (os.path.exists(LIB_PATH), LIB_PATH))
 
-        with jaydebeapi.connect('org.h2.Driver', [URI, ID, PW], LIB_PATH) as conn:
+        with jaydebeapi.connect('org.h2.Driver', URI, [ID, PW], LIB_PATH) as conn:
             with conn.cursor() as curs:
                 curs.execute("select * from MEMBERTBL")
                 data = curs.fetchall()
