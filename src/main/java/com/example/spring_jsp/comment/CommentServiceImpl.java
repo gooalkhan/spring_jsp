24124fa836fb26fac673ab879d032d75d5b36691
이현진 <ball811@naver.com>
2023-10-13 15:29:32 +0900

    20231013 댓글 삭제 구현, commentShow 수정, CommentServiceImpl의 import 수정

--- src/main/java/com/example/spring_jsp/comment/CommentServiceImpl.java
+++ src/main/java/com/example/spring_jsp/comment/CommentServiceImpl.java
@@ -6,8 +6,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
-import com.example.spring_jsp.member.MemberDTO;
-
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
